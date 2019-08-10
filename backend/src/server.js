e8175651442985e7802ae3758ebaf4b8fc5a371e
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-08-09 21:59:07 -0300

    add socket io

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,17 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+	const { user } = socket.handshake.query;
+	connectedUsers[user] = socket.id;
+	console.log('Nova conexão ', socket.id);
+});
 
 mongoose.connect(
 	'mongodb+srv://omnistack:omnistack@cluster0-ulikt.mongodb.net/omnistack8?retryWrites=true&w=majority',
@@ -13,8 +23,15 @@ mongoose.connect(
 	}
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+	req.io = io;
+	req.connectedUsers = connectedUsers;
+
+	return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css';
 import api from '../services/api';
@@ -6,9 +7,11 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
 	const [users, setUsers] = useState([]);
+	const [matchDev, setMatchDev] = useState(null);
 
 	useEffect(() => {
 		async function loadUsers() {
@@ -20,6 +23,16 @@ export default function Main({ match }) {
 			setUsers(response.data);
 		}
 		loadUsers();
+	}, [match.params.id]);
+
+	useEffect(() => {
+		const socket = io('http://localhost:3333', {
+			query: { user: match.params.id }
+		});
+
+		socket.on('match', dev => {
+			setMatchDev(dev);
+		});
 	}, [match.params.id]);
 
 	async function handleLike(id) {
@@ -67,6 +80,19 @@ export default function Main({ match }) {
 				</ul>
 			) : (
 				<div className="empty">Acabou :(</div>
+			)}
+
+			{matchDev && (
+				<div className="match-container">
+					<img src={itsamatch} alt="" />
+
+					<img className="avatar" src={matchDev.avatar} alt="" />
+					<strong>{matchDev.name}</strong>
+					<p>{matchDev.bio}</p>
+					<button type="button" onClick={() => setMatchDev(null)}>
+						FECHAR
+					</button>
+				</div>
 			)}
 		</div>
 	);
